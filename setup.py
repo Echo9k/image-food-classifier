0f2ea63cc3543c5042958376af5839e63aedf9d1
Guillermo Alcántara <32722692+Echo9k@users.noreply.github.com>
2024-01-24 14:57:06 -0600

    Initial file structure
    CookieCutter Data Science structure for the project as an initial template.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Deploying a Deep Learning Model on Web and Mobile Applications to identify the type of food in the image.',
+    author='Echo9k',
+    license='MIT',
+)
